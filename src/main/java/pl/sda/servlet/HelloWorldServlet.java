633049d91039ae2a09fb2d4028cf39d20af0b098
lkre <lukasz.res@gmail.com>
2020-08-23 13:36:21 +0200

    zadanie 26

--- src/main/java/pl/sda/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/servlet/HelloWorldServlet.java
@@ -1,5 +1,7 @@
 package pl.sda.servlet;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,9 +22,13 @@ public class HelloWorldServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+            throws IOException, ServletException {
         PrintWriter writer = response.getWriter();
         writer.println("newParam: " + getInitParameter("newParam"));
         writer.println("Localization: " + getInitParameter("localization"));
+
+
+                response.sendRedirect("empty.jsp");
+
     }
 }
